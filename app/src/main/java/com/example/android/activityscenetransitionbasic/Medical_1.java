ab69a8bd031bb2ef0528e347e9ab5b595a7b0921
rashidreyaz <rashidreyaz@gmail.com>
2020-09-05 16:35:33 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/android/activityscenetransitionbasic/Medical_1.java
@@ -0,0 +1,14 @@
+package com.example.android.activityscenetransitionbasic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Medical_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medical_1);
+    }
+}
